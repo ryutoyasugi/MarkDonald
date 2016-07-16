26f4ec5a51233b811bba8f21028e7471de87b3c9
ryutoyasugi <ry86tr2662@gmail.com>
2016-07-16 19:34:12 +0900

    minor modifications

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 var mainWindow = null;
 
-app.on('window-all-closed', function() {
-  if (process.platform !== 'darwin') app.quit();
-});
+/**
+ * main process
+ */
 app.on('ready', function() {
   mainWindow = new BrowserWindow({ width: 960, height: 594 });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+});
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') app.quit();
 });
